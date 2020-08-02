07d1aa04bc08e34bc686980696b9f38ba70710c4
firman <firman@sagita.tech>
2020-08-02 20:13:46 +0700

    changes biodata

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,6 +21,7 @@ class Footer extends Component {
            <ul className="copyright">
               <li>&copy; Copyright 2017 Tim Baker</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Edited by <a title="Styleshout" href="https://github.com/legenhand">Rizki Firmansyah</a></li>
            </ul>
 
         </div>
